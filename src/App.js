c561a6b428493893692ff7a7838b0b5a52495a98
MadhavanNagaraj2024 <madhavannagaraj.dev@gmail.com>
2026-01-02 15:40:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import bg from "./images/bg.jpg";
 function App() {
+  const [users, setUsers] = useState([]);
+  const API_URL = "http://localhost:5000/api/users";
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(API_URL);
+      const data = await res.json();
+      setUsers(data);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      <h2>MERN Simple App</h2>
+      <p>My app was running</p>
+      <img
+        src={bg}
+        style={{ objectFit: "cover", width: "100%", height: "671px" }}
+        alt="bg"
+      />
     </div>
   );
 }
